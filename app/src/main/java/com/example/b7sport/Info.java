fbc01c1dcbe06fc9ce963df7ff855196b8a7bd1e
odaiwa <45322451+odaiwa@users.noreply.github.com>
2020-04-10 18:12:09 +0300

    Add data to table and Authentication

---
+++ app/src/main/java/com/example/b7sport/Info.java
@@ -0,0 +1,47 @@
+package com.example.b7sport;
+
+public class Info {
+
+    String Email,PhoneNumber,Name,Password;
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public Info() {
+    }
+    public Info(String Email,String PhoneNumber,String Name,String Password) {
+        this.Email = Email;
+        this.Name = Name;
+        this.Password = Password;
+        this.PhoneNumber = PhoneNumber;
+    }
+}
--- app/src/main/java/com/example/b7sport/Register.java
+++ app/src/main/java/com/example/b7sport/Register.java
@@ -17,6 +17,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -26,6 +28,8 @@ public class Register extends AppCompatActivity {
     Button mRegisterbtn;
     TextView alreadyRegistered;
     FirebaseAuth fAuth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
     String regex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
 
     @Override
@@ -39,6 +43,8 @@ public class Register extends AppCompatActivity {
         mPhonenumber = findViewById(R.id.phoneNumber);
         mRegisterbtn = findViewById(R.id.Registerbtn);
         alreadyRegistered = findViewById(R.id.alreadyRegistred);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("EDMT_FIREBASE");
 
         fAuth = FirebaseAuth.getInstance();
 
@@ -54,11 +60,16 @@ public class Register extends AppCompatActivity {
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
                 String Name = mFullName.getText().toString().trim();
+                String PhoneNumber = mPhonenumber.getText().toString().trim();
                 if(EmailRequired(email)) return;
                 if(PasswordIsEmpty(password)) return;
                 if(PasswordLength(password)) return;
                 if(EmailRegex(email)) return;
                 if(CheckName(Name)) return;
+
+                Info info = new Info(email,PhoneNumber,Name,password);
+                databaseReference.push().setValue(info);
+
                 fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
