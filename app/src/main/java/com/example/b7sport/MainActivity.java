a4659eccd15b709ae9481f92808a587de5f436a9
Yaser <yaserni@ac.sce.ac.il>
2020-04-11 19:51:56 +0300

    PM2020T19-73
    Redirect to changePassword Page ..

--- app/src/main/java/com/example/b7sport/MainActivity.java
+++ app/src/main/java/com/example/b7sport/MainActivity.java
@@ -5,15 +5,32 @@ package com.example.b7sport;
         import android.content.Intent;
         import android.os.Bundle;
         import android.view.View;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
         import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mChange;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mChange = findViewById(R.id.chanpassmainv);
+      mChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),ChangePassword.class));
+            }
+        });
+
+
+
+
+
+
     }
 
         public void logout (View view){
