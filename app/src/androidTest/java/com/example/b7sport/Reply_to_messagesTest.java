42983e8f244659a2775f8908e7f8148a10fe9a27
AbdSkout <abdalsk@ac.sce.ac.il>
2020-06-12 23:42:42 +0300

    Create Reply_to_messagesTest.java

---
+++ app/src/androidTest/java/com/example/b7sport/Reply_to_messagesTest.java
@@ -0,0 +1,19 @@
+package com.example.b7sport;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Reply_to_messagesTest {
+
+    Reply_to_messages R;
+    @Before
+    public  void setup()
+    {
+         R=new Reply_to_messages();
+
+    }
+  
+
+}
